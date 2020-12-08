ab781f50901610949936cc7c5419fdeab6fff7f5
jun-note <jyunya.uchida@gmail.com>
2020-12-08 22:30:00 +0900

    微修正

--- main_nb.py
+++ main_nb.py
@@ -2,31 +2,30 @@ from os import X_OK
 import numpy as np
 
 import NaiveBayes.NB_classfier as NB
-from utils import AverageMeter, Logger, IrisDataset
+from utils import AverageMeter, IrisDataset
 
 if __name__ == '__main__':
-
-    ## dfine logger & meter
-    train_logger = Logger('train.log', ['epoch', 'loss', 'acc'])
-    test_logger = Logger('test.log', ['epoch', 'loss', 'acc_0', 'acc_1', 'acc_2'])
 
     accs = AverageMeter()
 
     ## data load
     iris = IrisDataset("data/iris.csv", norm=True)
-    train_data, test_data = iris.split()
+    train_data, test_data = iris.split(test_set_rate=1/5)
 
     model = NB.NaiveBayes(mode='gaussian')
 
     X = train_data[:, 0:4]
     Y = train_data[:, 4:7]
 
-    model.fit(X, Y)
+    model.fit(X, Y) ## training the model
 
-    pred = model.pred(test_data[:,0:4])
+    pred = model.pred(test_data[:,0:4]) ## prediction
+    
     for i, p in enumerate(pred):
+        flag = 0
+        print('{} \t {}'.format(p, test_data[i,4:7]))
         if np.argmax(p) == np.argmax(test_data[i,4:7]):
-            print('{} \t {}'.format(p, test_data[i,4:7]))
-            accs.update(1)
+            flag = 1
+        accs.update(flag)
 
-    print(accs.avg)
+    print('Acurracy : ', accs.avg)
